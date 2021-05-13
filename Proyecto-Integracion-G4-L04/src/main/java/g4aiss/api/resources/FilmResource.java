8651812c2541b32a72cf8d487de9605e7d5eaee9
Alvaro Gomez <alvgomnie@us.es>
2021-05-13 10:47:45 +0200

    Commit 10:47 Alvaro

--- Proyecto-Integracion-G4-L04/src/main/java/g4aiss/api/resources/FilmResource.java
+++ Proyecto-Integracion-G4-L04/src/main/java/g4aiss/api/resources/FilmResource.java
@@ -1,16 +1,27 @@
 package g4aiss.api.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.jboss.resteasy.spi.BadRequestException;
+import org.jboss.resteasy.spi.NotFoundException;
 
 import g4aiss.model.Film;
 import g4aiss.model.repository.BillboardRepository;
@@ -100,6 +111,64 @@ public class FilmResource {
 		}
 		
 		return result;
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces("application/json")
+	public Film get(@PathParam("id") String filmId)
+	{
+		Film f = repository.getFilm(filmId);
+		
+		if(f == null) {
+			throw new NotFoundException("The film with id="+ filmId +" was not found");	
+		}
+		
+		return f;
+	}
+	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response addSong(@Context UriInfo uriInfo, Film film) {
+		if(film.getTitle() == null || "".equals(film.getTitle())) {
+			throw new BadRequestException("The name of the film must not be null");
+		}
+		
+		repository.addFilm(film);
+		
+		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+		URI uri = ub.build(film.getId());
+		ResponseBuilder resp = Response.created(uri);
+		resp.entity(film);			
+		return resp.build();
+	}
+	
+	@PUT
+	@Consumes("application/json")
+	public Response updateSong(Film film) {
+		Film oldfilm = repository.getFilm(film.getId());
+		if (oldfilm == null) {
+			throw new NotFoundException("The playlist with id="+ film.getId() +" was not found");			
+		}
+		
+		// Update name
+		if (film.getTitle()!=null)
+			oldfilm.setTitle(film.getTitle());
+		
+		if(film.getDirector()!=null)
+			oldfilm.setDirector(film.getDirector());
+		
+		if(film.getDuration()!=null)
+			oldfilm.setDuration(film.getDuration());
+		
+		if(film.getGenre()!=null)
+			oldfilm.setGenre(film.getGenre());
+		
+		if(film.getRelease()!=null)
+			oldfilm.setRelease(film.getRelease());
+		
+		return Response.noContent().build();
 	}
 
 }
